c319300eed8ce8e4c862386e4f47748f59b2a0bd
SefikaBoz <sefikaboz12@gmail.com>
2017-05-22 19:11:56 +0300

    Create Addition.cs

---
+++ Addition.cs
@@ -0,0 +1,28 @@
+# 22-05-2017--2-
+
+using IPluginInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    public class Class1 : ICalculate
+    {
+        public int ToCalculate(int a, int b)
+        {
+            return a + b;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "toplama";
+            }
+        }
+        
+    }
+}
